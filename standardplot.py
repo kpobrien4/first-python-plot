65c03de46fbb561fbe29778b66d70c7743b54f1b
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-01 16:12:36 -0500

    new plot

--- standardplot.py
+++ standardplot.py
@@ -4,3 +4,7 @@ import numpy as np
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.exp(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
+
+y = np.linspace(0,20,100)
+plt.plot(y, np.sqrt(y))
+plt.show()
